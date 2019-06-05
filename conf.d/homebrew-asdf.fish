fa7a92f569754c77e94dd8887a665c8987e57fa6
Douglas Teoh <douglas@dteoh.com>
2019-06-05 09:24:41 +0900

    Update comments

--- conf.d/homebrew-asdf.fish
+++ conf.d/homebrew-asdf.fish
@@ -1,7 +1,10 @@
+# Most people don't change homebrew defaults, so the first test should
+# be pretty fast. If the defaults change, try bypassing the full
+# `brew --prefix <pkg>` bootup. Otherwise, get homebrew to do it all.
 if test -d /usr/local/opt/asdf
-  source /usr/local/opt/asdf/asdf.fish
+    source /usr/local/opt/asdf/asdf.fish
 else if test -d (brew --prefix)/opt/asdf
-  source (brew --prefix)/opt/asdf/asdf.fish
+    source (brew --prefix)/opt/asdf/asdf.fish
 else if test -d (brew --prefix asdf)
-  source (brew --prefix asdf)/asdf.fish
+    source (brew --prefix asdf)/asdf.fish
 end
