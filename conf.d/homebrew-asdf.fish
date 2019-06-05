52415e1759c18cc690f5858e93458a2c09e2cb21
Douglas Teoh <douglas@dteoh.com>
2019-06-05 09:21:22 +0900

    Try to improve performance

--- conf.d/homebrew-asdf.fish
+++ conf.d/homebrew-asdf.fish
@@ -1,3 +1,7 @@
-if test -d (brew --prefix asdf)
+if test -d /usr/local/opt/asdf
+  source /usr/local/opt/asdf/asdf.fish
+else if test -d (brew --prefix)/opt/asdf
+  source (brew --prefix)/opt/asdf/asdf.fish
+else if test -d (brew --prefix asdf)
   source (brew --prefix asdf)/asdf.fish
 end
