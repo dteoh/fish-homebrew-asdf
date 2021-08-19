0f9444dc64e9dcb13c9de4c198b1acee397d1e9e
Douglas Teoh <douglas@dteoh.com>
2021-08-19 09:55:31 +0900

    Support newest location of asdf.fish

--- conf.d/homebrew-asdf.fish
+++ conf.d/homebrew-asdf.fish
@@ -1,10 +1,10 @@
 # Most people don't change homebrew defaults, so the first test should
 # be pretty fast. If the defaults change, try bypassing the full
 # `brew --prefix <pkg>` bootup. Otherwise, get homebrew to do it all.
-if test -d /usr/local/opt/asdf
-    source /usr/local/opt/asdf/asdf.fish
-else if test -d (brew --prefix)/opt/asdf
-    source (brew --prefix)/opt/asdf/asdf.fish
+if test -d /usr/local/opt/asdf/libexec
+    source /usr/local/opt/asdf/libexec/asdf.fish
+else if test -d (brew --prefix)/opt/asdf/libexec
+    source (brew --prefix)/opt/asdf/libexec/asdf.fish
 else if test -d (brew --prefix asdf)
-    source (brew --prefix asdf)/asdf.fish
+    source (brew --prefix asdf)/libexec/asdf.fish
 end
