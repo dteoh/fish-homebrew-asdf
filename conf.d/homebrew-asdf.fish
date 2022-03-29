9eedc01e7273c848db6fd86ce98eb2927005f098
Douglas Teoh <douglas@dteoh.com>
2022-03-29 21:27:35 +0900

    Support Apple Silicon version of brew

--- conf.d/homebrew-asdf.fish
+++ conf.d/homebrew-asdf.fish
@@ -1,7 +1,9 @@
 # Most people don't change homebrew defaults, so the first test should
 # be pretty fast. If the defaults change, try bypassing the full
 # `brew --prefix <pkg>` bootup. Otherwise, get homebrew to do it all.
-if test -d /usr/local/opt/asdf/libexec
+if test -d /opt/homebrew/opt/asdf/libexec
+    source /opt/homebrew/opt/asdf/libexec/asdf.fish
+else if test -d /usr/local/opt/asdf/libexec
     source /usr/local/opt/asdf/libexec/asdf.fish
 else if test -d (brew --prefix)/opt/asdf/libexec
     source (brew --prefix)/opt/asdf/libexec/asdf.fish
